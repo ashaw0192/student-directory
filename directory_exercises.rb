b17cd890abefc24e3451d37ba9e170bd7d875d5f
Anthony Shaw <ashaw0192@gmail.com>
2022-11-06 16:22:55 +0000

    Solution to exercice 3: added ability to search for names less than n characters, and improved functionality

--- directory_exercises.rb
+++ directory_exercises.rb
@@ -27,10 +27,25 @@ end
 
 def print_beginning_with(students)
   puts "Student's names beginning with which letter should be printed?"
+  puts "To skip, just hit return"
   letter = gets.chomp.upcase
-  students.each do |student|
-    name_string = student[:name]
-    puts student if name_string[0] == letter
+  if !letter.empty?
+    students.each do |student|
+      name_string = student[:name]
+      puts student if name_string[0] == letter
+    end
+  end
+end
+
+def print_shorter_than_n_characters(students)
+  puts "Enter number to select students who's names have less characters"
+  puts "To skip, just hit return"
+  less_than = gets.chomp
+  if !less_than.empty?
+    students.each do |student|
+      name_string = student[:name]
+      puts student if name_string.delete(" ").length < less_than.to_i
+    end
   end
 end
 
@@ -39,3 +54,4 @@ print_header
 prints(students)
 print_footer(students)
 print_beginning_with(students)
+print_shorter_than_n_characters(students)

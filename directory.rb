3c559c3ec522536d8b52503dac31ce2ab69fdfc0
Anthony Shaw <ashaw0192@gmail.com>
2022-11-06 19:25:30 +0000

    Add interactive menu functionality with input, read and exit

--- directory.rb
+++ directory.rb
@@ -1,22 +1,10 @@
-students = [
-{name: "Dr. Hannibal Lector", cohort: :november},
-{name: "Darth Vader", cohort: :november},
-{name: "Nurse Ratchet", cohort: :november},
-{name: "Michael Corleone", cohort: :november},
-{name: "Alex DeLarge", cohort: :november},
-{name: "The Wicked Witch of the West", cohort: :november},
-{name: "Terminator", cohort: :november},
-{name: "Freddy Krueger", cohort: :november},
-{name: "The Joker", cohort: :november},
-{name: "Joffrey Baratheon", cohort: :november},
-{name: "Norman Bates", cohort: :november}
-]
+@students = []
 
 def print_header
   puts "The students of Villains Academy\n--------------"
 end
 
-def prints(students)
+def print_student_list(students)
   students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
@@ -29,17 +17,47 @@ end
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  students = []
   name = gets.chomp
   while !name.empty? do
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
     name = gets.chomp
   end
-  students
+  @students
 end
 
-students = input_students
-print_header
-prints(students)
-print_footer(students)
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_student_list(@students)
+  print_footer(@students)
+end
+
+def process(selection)
+  case selection
+    when "1"
+      @students = input_students
+    when "2"
+      show_students
+    when "9"
+      exit 
+    else
+      puts "I don't know what you meant, try again"
+  end
+end
+
+
+interactive_menu
+

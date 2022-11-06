8879500fc0e2a3d232f67bbad9078f9998d569ff
Anthony Shaw <ashaw0192@gmail.com>
2022-11-06 16:45:06 +0000

    Solution to exercise 4 and added notes to help difrentiate sections and exercises

--- directory_exercises.rb
+++ directory_exercises.rb
@@ -1,17 +1,30 @@
+#Prints the header for final list
 def print_header
   puts "The students of Villains Academy\n--------------"
 end
 
+#(Exercise 1) Prints each individual student using each loop and with index 
 def prints(students)
   students.each_with_index do |student, idx|
     puts "#{idx +1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
+#(Exercise 4) Alternate print method using while loop
+def prints_while(students)
+  counter = 0
+  while counter != students.count
+    puts students[counter]
+    counter += 1
+  end
+end
+
+#Prints the footer for final list with count
 def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
 
+#Takes user input of students 
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
@@ -25,6 +38,7 @@ def input_students
   students
 end
 
+#(Exercise 2) Prints students beginning with given letter
 def print_beginning_with(students)
   puts "Student's names beginning with which letter should be printed?"
   puts "To skip, just hit return"
@@ -37,6 +51,7 @@ def print_beginning_with(students)
   end
 end
 
+#(Exercise 3) Prints students whose names have less than n characters
 def print_shorter_than_n_characters(students)
   puts "Enter number to select students who's names have less characters"
   puts "To skip, just hit return"
@@ -49,6 +64,7 @@ def print_shorter_than_n_characters(students)
   end
 end
 
+#for running the program
 students = input_students
 print_header
 prints(students)

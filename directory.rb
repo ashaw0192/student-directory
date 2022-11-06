ab696448b1203b2ba9291e24f72dc738ec236147
Anthony Shaw <ashaw0192@gmail.com>
2022-11-06 20:02:59 +0000

    Add functionality to load student list on startup if specified

--- directory.rb
+++ directory.rb
@@ -17,7 +17,7 @@ end
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  name = gets.chomp
+  name = STDIN.gets.chomp
   while !name.empty? do
     @students << {name: name, cohort: :november}
     puts "Now we have #{@students.count} students"
@@ -30,7 +30,7 @@ def interactive_menu
   File.open(".gitignore", "w") << "students.csv"
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
@@ -75,8 +75,8 @@ def save_students
   file.close
 end
 
-def load_students
-  file = File.open("students.csv", "r")
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
   file.readlines.each do |line|
   name, cohort = line.chomp.split(',')
     @students << {name: name, cohort: cohort.to_sym}
@@ -84,6 +84,18 @@ def load_students
   file.close
 end
 
+def try_load_students
+    filename = ARGV.first 
+    return if filename.nil?
+    if File.exist?(filename) 
+      load_students(filename)
+       puts "Loaded #{@students.count} from #{filename}"
+    else 
+      puts "Sorry, #{filename} doesn't exist."
+      exit 
+    end
+  end
 
+try_load_students
 interactive_menu
 

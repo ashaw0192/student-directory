b577dce5873ff45052efa8ceb21de40263505f39
Anthony Shaw <ashaw0192@gmail.com>
2022-11-06 15:55:47 +0000

    created copy of directory.rb and solution for exercise 1: puts with index

---
+++ directory_exercises.rb
@@ -0,0 +1,31 @@
+def print_header
+  puts "The students of Villains Academy\n--------------"
+end
+
+def prints(students)
+  students.each_with_index do |student, idx|
+    puts "#{idx +1}. #{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students"
+end
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  students = []
+  name = gets.chomp
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  students
+end
+
+students = input_students
+print_header
+prints(students)
+print_footer(students)
